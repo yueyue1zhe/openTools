90f270e5e886939df15835abf70ffb95e4e09646
theodore <zhouzhe5934@163.COM>
2022-07-30 00:50:57 +0800

    snippet comm update db

---
+++ snippet/vue3ts/components/action-icon/ActionIcon.vue
@@ -0,0 +1,38 @@
+<template>
+  <app-icon
+    class="icon-action y-pointer"
+    :size="prop.size"
+    :name="prop.name"
+    :color="prop.color"
+    @click="emit('click')"
+  ></app-icon>
+</template>
+
+<script setup lang="ts">
+import AppIcon from "@/components/icon/AppIcon.vue";
+
+interface actionIconPropsType {
+  size?: string;
+  name: string;
+  color?: string;
+}
+
+const prop = withDefaults(defineProps<actionIconPropsType>(), {
+  size: "16",
+  color: "",
+});
+
+const emit = defineEmits<{
+  (e: "click"): void;
+}>();
+</script>
+
+<style lang="scss" scoped>
+.icon-action {
+  color: var(--color-placeholder);
+}
+
+.icon-action:hover {
+  color: var(--color-text-primary);
+}
+</style>
