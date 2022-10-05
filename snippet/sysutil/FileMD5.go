d437f8f6bfb751b551a5e4b974a9f28e3c9a4a27
theodore <zhouzhe5934@163.COM>
2022-10-05 23:08:26 +0800

    snippet comm update db

---
+++ snippet/sysutil/FileMD5.go
@@ -0,0 +1,18 @@
+package fileutil
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"io"
+	"os"
+)
+
+func FileMD5(filePath string) (string, error) {
+	file, err := os.Open(AttachmentPath(filePath))
+	if err != nil {
+		return "", err
+	}
+	hash := md5.New()
+	_, _ = io.Copy(hash, file)
+	return hex.EncodeToString(hash.Sum(nil)), nil
+}
