67c0544f9ba1fb5a28719fcb982395b2a23bd67e
theodore <zhouzhe5934@163.COM>
2022-12-25 16:59:11 +0800

    snippet comm update db

---
+++ snippet/sysutil/FastWorker.go
@@ -0,0 +1,21 @@
+package core
+
+import "sync"
+
+func fastWorkerDo(wg *sync.WaitGroup, fn func()) {
+	fn()
+	wg.Done()
+}
+
+func FastWorker(fn ...func()) {
+	fnNum := len(fn)
+	if fnNum < 0 {
+		return
+	}
+	var wg sync.WaitGroup
+	wg.Add(fnNum)
+	for _, f := range fn {
+		go fastWorkerDo(&wg, f)
+	}
+	wg.Wait()
+}
