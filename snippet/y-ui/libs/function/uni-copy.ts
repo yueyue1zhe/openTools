b2bb9aabfb375265f63017622ba2dbd5f808d928
theodore <zhouzhe5934@163.COM>
2023-02-10 15:37:54 +0800

    snippet comm update ui

---
+++ snippet/y-ui/libs/function/uni-copy.ts
@@ -0,0 +1,61 @@
+type uniCopyOpt = {
+    content: string | [] | number | object
+    success: VoidCallBack
+    error: VoidCallBack
+}
+
+export default (content: string) => {
+    return new Promise((resolve: VoidCallBack, reject: VoidCallBack) => {
+        uniCopy({
+            content: content,
+            success: () => {
+                console.log("clipboard success");
+                resolve()
+            },
+            error: (e) => {
+                console.warn("clipboard fail", e);
+                reject(e)
+            }
+        })
+    })
+}
+
+function uniCopy({content, success, error}: uniCopyOpt) {
+
+    content = typeof content === 'string' ? content : content.toString()
+
+    //#ifndef H5
+    uni.setClipboardData({
+        data: content,
+        success: function () {
+            success("复制成功~")
+            console.log('success');
+        },
+        fail: function () {
+            error("复制失败~")
+        }
+    });
+    //#endif
+
+    /**
+     * H5端的复制逻辑
+     */
+    // #ifdef H5
+    if (!document.queryCommandSupported('copy')) {
+        error('浏览器不支持')
+    }
+    let textarea = document.createElement("textarea")
+    textarea.value = content
+    textarea.readOnly = true
+    document.body.appendChild(textarea)
+    textarea.select()
+    textarea.setSelectionRange(0, content.length)
+    let result = document.execCommand("copy")
+    if (result) {
+        success("复制成功~")
+    } else {
+        error("h5安全拦截")
+    }
+    textarea.remove()
+    // #endif
+}
