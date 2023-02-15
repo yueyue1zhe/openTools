61591e8f72a713c8fa9397000807afd00b001217
theodore <zhouzhe5934@163.COM>
2023-02-15 11:11:23 +0800

    snippet comm update ui

---
+++ snippet/y-ui/components/YImage/YImage.vue
@@ -0,0 +1,30 @@
+<template>
+  <image
+      :src="props.src"
+      :style="props.yStyle"
+      :class="yClass"
+      :mode="props.mode"
+      show-menu-by-longpress
+      :draggable="false"
+      style="display: flex"
+  ></image>
+</template>
+
+<script lang="ts" setup>
+
+const props = withDefaults(defineProps<{
+  src: string,
+  yStyle: string,
+  yClass: string,
+  mode?: "scaleToFill" | "aspectFit" | "aspectFill" | "widthFix" | "heightFix",
+}>(), {
+  src: "",
+  yStyle: "max-width:100%;",
+  yClass: "",
+  mode: "scaleToFill",
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
