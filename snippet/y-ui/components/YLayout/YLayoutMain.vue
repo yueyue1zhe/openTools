c7783b76a567b9e1c5714208b8871285a5b9e8bf
theodore <zhouzhe5934@163.COM>
2023-03-03 11:32:11 +0800

    snippet comm update ui

---
+++ snippet/y-ui/components/YLayout/YLayoutMain.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="layout-main-box">
+    <slot name="header"></slot>
+    <view class="y-flex-1">
+      <slot></slot>
+    </view>
+    <slot name="footer"></slot>
+  </view>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+.layout-main-box {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
