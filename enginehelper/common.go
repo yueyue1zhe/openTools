657bad18bc067121a9a859dd119ac5f9be2fb4a6
zhou <zhouzhe5934@hotmail.com>
2024-03-10 23:59:28 +0800

    snippet comm update ui

---
+++ enginehelper/common.go
@@ -0,0 +1,29 @@
+package tests
+
+import (
+	"d-photo/app"
+	"d-photo/env"
+	"e.coding.net/zhechat/magic/taihao/core"
+	"e.coding.net/zhechat/magic/taihao/library/logutil"
+)
+
+func withDb() {
+	logutil.Init()
+	conf := env.GetConf()
+	appConf := core.Conf{
+		Name:                  env.AppName,
+		Version:               "0.1.10",
+		AttachmentPath:        "/attachment",
+		JWTTokenKey:           env.JWTTokenKey,
+		StructBasic:           env.StructBasic,
+		MultiTenantIDTokenKey: env.MultiTenantIDTokenKey,
+		MultiTenantIDQueryKey: env.MultiTenantIDQueryKey,
+		Debug:                 conf.Debug,
+		DevProxySiteDomain:    conf.DevProxySiteDomain,
+		MultiTenant:           true,
+	}
+	core.Init(appConf)
+	if err := app.RegisterByConf(); err != nil {
+		panic("启动异常：" + err.Error())
+	}
+}
